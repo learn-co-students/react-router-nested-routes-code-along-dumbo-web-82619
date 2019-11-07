452f0ab76924ec6468bfb44417dec32a47775931
dangrammer <52326502+dangrammer@users.noreply.github.com>
2019-11-06 20:43:20 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './containers/App';
+import React from 'react'
+import {render} from 'react-dom'
+import App from './containers/App'
 
 render(
-  <App />,
+  <App/>,
   document.getElementById('root')
-);
+)
